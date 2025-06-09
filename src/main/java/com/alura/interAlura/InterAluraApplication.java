4ddf78bf2d3c96e4c35ebf694329db243972a6ab
maedson lima <maedsonrhcp@gmail.com>
2025-06-09 01:25:28 -0300

    Projeto base

---
+++ src/main/java/com/alura/interAlura/InterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.interAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterAluraApplication.class, args);
+	}
+
+}
